83eb5acfcf1c3eeb837f8ba84031e3a8c2c4d02a
Sunandinighosh <78850319+Sunandinighosh@users.noreply.github.com>
2021-05-24 17:01:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+menu_list_array = ["Veg Margherita Pizza",
+"CHICKEN TANDOORI PIZZA",
+"VEG SUPREME PIZZA",
+"PANEER TIKKA PIZZA",
+"DELUXE VEGGIE PIZZA",
+"VEG EXTRAVAGANZA PIZZA",
+"CHICKEN SAUSAGE PIZZA",
+"EXTRA CHEESE PIZZA"];
+                    
+
+function getmenu(){
+var htmldata;
+htmldata="<ol class='menulist'>"
+menu_list_array.sort();
+for(var i=0;i<menu_list_array.length;i++){
+    htmldata=htmldata+'<li>'+menu_list_array[i]+'</li>'
+}
+htmldata=htmldata+"</ol>"
+document.getElementById("display_menu").innerHTML=htmldata;
+}
+
+function add_item(){
+var htmldata;
+var item=document.getElementById("add_item").value;
+menu_list_array.sort();
+htmldata="<section class='cards'>"
+for(var i=0;i<menu_list_array.length;i++){
+    htmldata=htmldata+'<div class="card">'
++'<img src="pizzaImg.png"/>'
++menu_list_array[i]+'</div>'
+}
+htmldata=htmldata+"</section>"
+document.getElementById("display_addmenu").innerHTML=htmldata;
+}
+
+function add_top(){
+    var item=document.getElementById("add_item").value;
+    menu_list_array.push(item);
+    add_item();
+}
